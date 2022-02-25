39d9fed9ed3c876db826147ec8ec3827943a40aa
composedMender <68520270+composedMender@users.noreply.github.com>
2022-02-25 05:22:18 -0500

    created audioreactive functionality and organized

--- scripts/anima.js
+++ scripts/anima.js
@@ -1,32 +1,61 @@
 import * as THREE from '/vendor/three/build/three.module.js';
+import { LineMaterial } from '/vendor/three/examples/jsm/lines/LineMaterial.js';
+import { Wireframe } from '/vendor/three/examples/jsm/lines/Wireframe.js';
+import { WireframeGeometry2 } from '/vendor/three/examples/jsm/lines/WireframeGeometry2.js';
 
-function main() {
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer( { canvas } );
+import { EffectComposer } from '/vendor/three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from '/vendor/three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from '/vendor/three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
-    const fov = 75;
-    const aspect = 2;
-    const near = 0.1;
-    const far = 5;
-    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+let canvas, renderer, scene, camera, analyzer, uniforms;
 
+const fov = 75;
+const aspect = 2;
+const near = 0.1;
+const far = 1000;
+
+let cube;
+let line;
+
+let d, o;
+let material;
+
+let wireframe;
+let composer;
+
+const params = {
+    exposure: 1,
+    bloomStrength: 3.5,
+    bloomThreshold: 0,
+    bloomRadius: 1
+};
+
+init();
+animate();
+
+function init() {
+
+    const fftSize = 512;
+
+    //
+
+    canvas = document.querySelector('#c');
+    renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(canvas.clientWidth, canvas.clientHeight / 2 );
+    
+    scene = new THREE.Scene();
+    
+    camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
     camera.position.z = 2;
+    const renderScene = new RenderPass( scene, camera );
 
-    const scene = new THREE.Scene();
+    const bloomPass = new UnrealBloomPass( new THREE.Vector2( canvas.clientWidth, canvas.clientHeight ), 1.5, 0.4, 0.85 );
+    bloomPass.threshold = params.bloomThreshold;
+    bloomPass.strength = params.bloomStrength;
+    bloomPass.radius = params.bloomRadius;
 
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
-    const material = new THREE.MeshPhongMaterial( { color : 0x44aa88 } );
-
-    const cubes = [
-        makeInstance( geometry, 0x44aa88, 0 ),
-        makeInstance( geometry, 0x8844aa, -2 ),
-        makeInstance( geometry, 0xaa8844, 2 )
-    ];
-
-    renderer.render(scene, camera);
+    //lighting
 
     {
         const color = 0xFFFFFF;
@@ -34,70 +63,102 @@ function main() {
         const light = new THREE.DirectionalLight( color, intensity );
         light.position.set( -1, 2, 4 );
         scene.add( light );
+
+        scene.add( new THREE.AmbientLight( 0x404040 ) );
     }
 
-    function makeInstance( geometry, color, x ) {
-        const material = new THREE.MeshPhongMaterial( { color } );
+    //
 
-        const cube = new THREE.Mesh( geometry, material)
-        scene.add(cube);
-        cube.position.x = x;
-        return cube;
-    }
+    composer = new EffectComposer( renderer );
+    composer.addPass( renderScene );
+    composer.addPass( bloomPass );
 
-    function resizeRendererToDisplaySize( renderer ) {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const needResize = canvas.width !== width || canvas.height !== height;
-        if ( needResize ) {
-            renderer.setSize( width, height, false );
-        }
-        return needResize;
-    }
+    const listener = new THREE.AudioListener();
+    const sound = new THREE.Audio( listener );
+    const file = '../resources/deo.mp3';
 
-    function render( time ) {
-        time *= 0.001;
+    const audioLoader = new THREE.AudioLoader();
+    audioLoader.load( file, function( buffer ) {
+        sound.setBuffer( buffer );
+        sound.setLoop( true );
+        sound.setVolume( 0.5 );
+        sound.play();
+    });
 
-        if (resizeRendererToDisplaySize(renderer)) {
-            const canvas = renderer.domElement;
-            camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.updateProjectionMatrix();
-        }
+    analyzer = new THREE.AudioAnalyser( sound, fftSize );
 
-        cubes.forEach(( cube, ndx ) => {
-            const speed = 1 + ndx * 0.1;
-            const rot = time * speed;
-            cube.rotation.x = rot;
-            cube.rotation.y = rot
-        });
+    //
 
-        renderer.render( scene, camera );
+    const power = new Float32Array( 160 );
+    const cubeMat = new THREE.MeshPhongMaterial();
+    const cubeGeo = new THREE.IcosahedronGeometry( 1, 0 );
+
+    const format = ( renderer.capabilities.isWebGL2 ) ? THREE.RedFormat : THREE.LuminanceFormat;
+
+    uniforms = {
+        vertPos: { value: 55.0 },
+        tAudioData: { value: analyzer.getFrequencyData().reduce((prev, curr) => prev + curr, 0) }
+    };
+
+
+    // central icosahedron //
+
+    let icoGeo = new THREE.IcosahedronGeometry( 1, 0 );
+    const wireGeo = new THREE.WireframeGeometry( icoGeo );
+    let matLine = new THREE.LineBasicMaterial( {
+        color: 0x4080ff,
+        linewidth: 1
+    });
+
+    wireframe = new THREE.LineSegments( wireGeo );
+    wireframe.material.depthTest = false;
+    wireframe.material.transparent = true;
+    scene.add( wireframe );
+
+    //
+
     
-        requestAnimationFrame( render );
-    }
-    
-    requestAnimationFrame(render);
+
 }
 
-
-main();
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+function resizeRendererToDisplaySize( renderer ) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if ( needResize ) {
+        renderer.setSize( width, height, false );
     }
-  });
-} 
+    return needResize;
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+    render();
+
+}
+
+function render() {
+
+    d = analyzer.getFrequencyData();
+    o = d.reduceRight((prev, curr) => prev + curr, 240);
+
+    uniforms.tAudioData.value = o;
+    uniforms.tAudioData.needsUpdate = true;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+
+    params.bloomStrength = (o * 0.1 / d.length * 0.02) * 0.4;
+
+    wireframe.rotation.x += (o * 0.1 / d.length * 0.02) * 0.1;
+    wireframe.rotation.y += (o * 0.1 / d.length * 0.02) * 0.1;
+    wireframe.scale.setScalar(
+        o * 0.1 / d.length * 0.15);
+
+    composer.render( scene, camera );
+}
